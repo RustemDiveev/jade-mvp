1bcc1efc9bf0a2a4b7fcca22f995eac7c9822a2a
Rustem Diveev <rustem.diveev@gmail.com>
2024-07-27 12:11:27 +0300

    new: initial commit

---
+++ src/main/java/com/reu/jade_mvp/Jade_mvp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.reu.jade_mvp;
+
+import jade.core.Agent;
+
+/**
+ *
+ * @author ruste
+ */
+public class Jade_mvp extends Agent {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
